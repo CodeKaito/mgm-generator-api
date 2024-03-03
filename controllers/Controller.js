84c77fd6c010180a665e50863e40b429eda7b5c4
CodeKaito <jerome.decinco@maxwell.mi.it>
2024-03-03 10:44:43 +0100

    update

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -1,22 +1,36 @@
 const MGMDataModel = require("../models/Model");
 
 module.exports.getMGMData = async (req, res) => {
-    const mgmData = await MGMDataModel.find();
-    res.send(mgmData);
+    try {
+        // Esegui la logica della tua richiesta di fetch qui
+        const mgmData = await MGMDataModel.find();
 
-    // res.send("Hello, world!");
+        // Incrementa il count e gestisci isValid per ciascun oggetto fetchato
+        for (const mgmObject of mgmData) {
+            mgmObject.count++;
+            if (mgmObject.count === 5) {
+                mgmObject.isValid = false;
+            }
+            await mgmObject.save(); // Salva le modifiche nel database
+        }
+
+        res.send(mgmData);
+    } catch (error) {
+        console.error('Errore nella richiesta di fetch:', error.message);
+        res.status(500).json({ error: 'Something went wrong!' });
+    }
 }
 
 module.exports.saveMGMData = (req, res) => {
     const { mgmData } = req.body;
 
-    MGMDataModel.create({mgmData})
+    MGMDataModel.create({ mgmData })
         .then((data) => {
             console.log("Saved successfully");
             res.status(201).send(data);
         })
         .catch((error) => {
             console.log(error.message);
-            res.send({error: error.message, msg: "Something went wrong!"});
+            res.status(500).send({ error: error.message, msg: "Something went wrong!" });
         });
 }
--- models/Model.js
+++ models/Model.js
@@ -4,7 +4,15 @@ const mgmSchema = new mongoose.Schema({
     mgmData: {
         type: String,
         required: true,
-    }
+    },
+    count: {
+        type: Number,
+        default: 0, // Inizializza il count a 0
+    },
+    isValid: {
+        type: Boolean,
+        default: true, // Inizializza isValid a true
+    },
 })
 
 module.exports = mongoose.model("mgmData", mgmSchema);
